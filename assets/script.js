1e4e09a44ce42de6c0461d1475b9188b00cf6cfe
thiago <nasciutti.thiago@gmail.com>
2022-09-01 19:23:10 -0400

    save

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,16 @@ var display3El = document.querySelector(".display-3");
 var leadEl = document.querySelector(".lead");
 var containerEl = document.querySelector(".container");
 var currentDayEl = document.querySelector("#currentDay");
+var txt9El = document.querySelector("#txt9");
+var btn9El = document.querySelector("#btn9");
 
-//Display the current day
+//Display the Current Day
 currentDayEl.textContent = moment().format("dddd, MMMM Do");
+
+//Save text
+function save() {
+  console.log(txt9.value);
+}
+
+//Listeners
+btn9El.addEventListener("click", save);
